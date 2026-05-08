06d2945f66715a833000838d810f56ba60e783bd
viswaksen <vishwaksen1998@gmail.com>
2026-05-09 00:47:05 +0530

    Working

--- commands.sh
+++ commands.sh
@@ -2,10 +2,12 @@
 
 USERID=$(id -u)
 
-if [ $USERID !=0 ]
+if [ $USERID -ne 0 ]
 then
     echo "EROOR: Please run the script with root access"
     exit 0
 else
     echo "You are running with root access"
 fi
+
+dnf install mysql -y
